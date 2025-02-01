6ded5b18ae33fe407843466ce69344650f2a4d7d
Scrambled <smartsnakie15@gmail.com>
2025-01-31 19:12:20 -0500

    coin logic added

---
+++ coin.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+
+#when coin is picked up
+signal grabbed 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	$coinSprite.play()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	pass # Replace with function body.
+	queue_free()
+	
+
+
+func _on_body_entered(body: Node) -> void:
+	pass # Replace with function body.
+	
+	hide() #hide coin
+	grabbed.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
--- main.gd
+++ main.gd
@@ -1,7 +1,10 @@
 extends Node
 
 @export var mob_scene: PackedScene
+@export var coin_scene: PackedScene
+
 var score
+var coinScore
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,6 +21,7 @@ func game_over() -> void:
 	$deathSound.play()
 	$scoreTimer.stop()
 	$mobTimer.stop()
+	$coinTimer.stop()
 	$HUD.show_game_over()
 	
 func new_game():
@@ -29,7 +33,6 @@ func new_game():
 	$HUD.show_message("prepare yourself...")
 	$Player.start($startPosition.position)
 	$startTimer.start()
-	
 	
 
 
@@ -69,5 +72,30 @@ func _on_start_timer_timeout() -> void:
 	pass # Replace with function body.
 	$mobTimer.start()
 	$scoreTimer.start()
+	$coinTimer.start()
+
+#manages coin spawning
+func _on_coin_timer_timeout() -> void:
+	pass # Replace with function body.
+	var coin = coin_scene.instantiate()
+	
+	#random path2d point
+	var coin_spawn_location = $coinPath/coinSpawns
+	coin_spawn_location.progress_ratio = randf()
+	
+	#setting spawn direction?
+	var direction = coin_spawn_location.rotation + PI/2
+	
+	#setting position to something random
+	coin.position = coin_spawn_location.position
+	
+	#no rotation spawn randomness
+	
+	#setting velocity
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	coin.linear_velocity = velocity.rotated(direction)
+	
+	add_child(coin)
+	
 	
 	
